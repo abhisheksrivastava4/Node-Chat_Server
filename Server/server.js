233c7c93a40fd01ae2ac3d8b62f5578e6e7e2aa0
Abhishek Srivastava <abhishek.srivastava4@gmail.com>
2018-06-26 11:54:35 +0530

    modified package.json

--- Server/server.js
+++ Server/server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 3000;
 
 const app = new express();
 
-var fullPath = path.join(__dirname,'/../public');
+var fullPath =   path.join(__dirname,'/../public');
 
 console.log(fullPath);
 
-app.use(express.static(fullPath));
+app.use(express.static(fullPath)); 
 
 app.listen(port,()=>{
     console.log(`server up on port ${port}`);
