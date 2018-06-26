d1bd872c3650ff4e2038e39b75543d069ffb3593
Abhishek Srivastava <abhishek.srivastava4@gmail.com>
2018-06-26 10:29:03 +0530

    Inital Commit

---
+++ Server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const app = new express();
+
+var fullPath = path.join(__dirname,'/../public');
+
+console.log(fullPath);
+
+app.use(express.static(fullPath));
+
+app.listen(port,()=>{
+    console.log(`server up on port ${port}`);
+});
